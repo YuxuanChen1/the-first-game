59b387f2fd5d9742e1254fada2546ec36a7dc9ee
YuxuanChen1 <2580069953@qq.com>
2021-04-05 19:58:57 +0800

    第八天

---
+++ Assets/1script/UI/UIController_WinMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController_WinMenu : MonoBehaviour
+{
+    public void SwicthToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
